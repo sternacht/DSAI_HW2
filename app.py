2be31d2e3468bcbffb38095ea9b3cadd597a3dc8
sternacht09 <e94076186@gs.ncku.edu.tw>
2022-04-24 14:00:55 +0800

    4/24 update model_load

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from pickle import FALSE, TRUE
 import pandas as pd
+from keras.models import load_model
 from lstm import MODEL
 
 if __name__ == '__main__':
@@ -16,13 +18,21 @@ if __name__ == '__main__':
     parser.add_argument('--output',
                         default='output.csv',
                         help='output file name')
+    parser.add_argument('--model_load',
+                        default=FALSE,
+                        help='use trained model if TRUE')
     args = parser.parse_args()
     
     # The following part is an example.
     # You can modify it at will.
     LSTM = MODEL()
     train_x, train_y = LSTM.training_process(args.training)
-    model, offset = LSTM.model_training(train_x, train_y)
+    if args.model_load == FALSE:
+        model, offset = LSTM.model_training(train_x, train_y)
+    else:
+        model = load_model('lstm_model')
+        print("model loaded.")
+        offset = LSTM.compute_offset(model, train_x, train_y)
     test_x = LSTM.testing_process(args.testing, args.training)
     action = LSTM.make_pred(test_x, offset, model)
     
